b3c43db96579d1a43a141ec9cf06835628c15552
Leo Alves <leonardo.filipe.alves@gmail.com>
2017-08-30 21:47:35 +0200

    structure is a good thing

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+let nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+    entry: ['babel-polyfill', './handler.js'],
+    target: 'node',
+    devtool: 'source-map',
+    externals: [nodeExternals()],
+    module: {
+        preLoaders: [{
+            test: /\.js$/,
+            exclude: [
+                'node_modules/',
+                /_serverless.*$/,
+            ],
+            loader: 'eslint-loader',
+        }],
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                include: __dirname,
+                exclude: [
+                    /node_modules/,
+                ],
+            },
+        ],
+    },
+}
