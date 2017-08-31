c1d7469b9480ccb943b5b9ef4df4762d7493e608
Leo Alves <leonardo.filipe.alves@gmail.com>
2017-08-31 23:14:11 +0200

    readbalance working

--- handler.js
+++ handler.js
@@ -5,6 +5,10 @@ const { ACCESS_KEY } = process.env
 const messageBirdClient = messageBird(ACCESS_KEY)
 
 export function sendSms(event, context, callback) {
+
+}
+
+export function readBalance(event, context, callback) {
   messageBirdClient.balance.read( (err, data) => {
     callback(err, data)
   })
