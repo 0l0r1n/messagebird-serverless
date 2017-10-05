e793df9b07f190617b10f9f1c6c75bc1e2bfc87b
Leo Alves <leonardo.filipe.alves@gmail.com>
2017-10-05 09:10:42 +0200

    syntax adjustments

--- handler.js
+++ handler.js
@@ -6,16 +6,11 @@ const messageBirdClient = messageBird(ACCESS_KEY)
 
 export function sendSms(event, context, callback) {
   const body = JSON.parse(event.body)
-  body.messagesToSend.map(messageToSend => {
-    console.log(messageToSend)
+  body.messagesToSend.map(messageToSend => { 
     const { originator, recipients, body } = messageToSend
     messageBirdClient.messages.create({ originator, recipients, body }, (err, data) => {
-      if (err) callback(err, {
-        statusCode: 400
-      })
-      else callback(null, {
-        statusCode: 204
-      })
+      if (err) callback(err, { statusCode: 400 })
+      else callback(null, { statusCode: 204 })
     })
   })
 }
